07d6cadae1afd5bcbd6f94fbd84d402efd5ddfb7
roma_gud <roma@nominal.so>
2026-03-28 11:52:12 +0400

    feat: LM-003 Search tool

--- main.py
+++ main.py
@@ -1,10 +1,44 @@
+from pprint import pprint
+
 from dotenv import load_dotenv
-import os
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from tavily import TavilyClient
 
 load_dotenv()
 
+tavily = TavilyClient()
+
+
+@tool
+def search(query: str) -> str:
+    """
+    Tool that searches weather over the internet.
+    Args:
+        query: The query to search for.
+    Returns:
+        The search result.
+    """
+    print(f"Searching for: {query}")
+    return tavily.search(query)
+
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite", temperature=0.3)
+tools = [search]
+agent = create_agent(model=llm, tools=tools)
+
+
 def main():
-    print(os.getenv("GOOGLE_API_KEY"))
+    print("Running the agent...")
+    result = agent.invoke(
+        {"messages": HumanMessage(
+            content="Search 3 job postings for an ai engineer in Georgia, California and Washington on Linkedin with a list of details."
+        )}
+    )
+    pprint(result)
+
 
 if __name__ == "__main__":
     main()
